1e47ea2130c8207b5c1b5964abcdb0cc4b978562
Aditya Yadav <ayadav@cs.umass.edu>
2017-07-24 19:40:31 -0400

    Some fixes to log copy script

--- scripts/copyGNSLogsOnTestFail.sh
+++ scripts/copyGNSLogsOnTestFail.sh
@@ -1,18 +1,26 @@
 #!/bin/bash
 # This script should be called from top-level GNS directory, which has build.xml file.
 
+dataDir="/home/GNSDir"
+
+echo $dataDir
+cd $dataDir/GNS
+echo "pwd "
+pwd
+
+
 # location where logs will be stored
-dataDir="/home/GNSDir/failedLogs"
+logDir=$dataDir"/failedLogs"
 # Some sort of Id that will be used to idetify and number logs.
 failureId=$1
 
 # making the directory if it doesn't exist.
-mkdir $dataDir
+mkdir $logDir
 
 #mkaing new directory to store current logs
-mkdir $dataDir/logs-$failureId
+mkdir $logDir/logs-$failureId
 
-cp -rf logs/* $dataDir/logs-$failureId/.
+cp -rf logs/* $logDir/logs-$failureId/.
 
 echo "Copy of logs complete on "$HOSTNAME" failureId="$failureId
 
